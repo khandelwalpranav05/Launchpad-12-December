818c0745a58f60c87986879e34f09a15309b577a
Pranav Khandelwal <pranavkhandelwal@Pranavs-MacBook-Air.local>
2019-12-14 16:53:49 +0530

    loops and pattern

--- Lecture 2/pattern5.cpp
+++ Lecture 2/pattern5.cpp
@@ -4,7 +4,25 @@ using namespace std;
 
 int main(){
 
-	
+	int n = 5;
+
+	for(int row =1;row<=n;row++){
+
+		for(int space = 1;space<=n-row;space++){
+			cout<<" ";
+		}
+
+		for(int dec = row;dec>=1;dec--){
+			cout<<dec;
+		}
+
+		for(int inc=2;inc<=row;inc++){
+			cout<<inc;
+		}
+
+		cout<<endl;
+
+	}	
 
 	return 0;
 }
