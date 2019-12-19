818c33af3d9beba512697abbd38a4a0a894986a0
Pranav Khandelwal <pranavkhandelwal@Pranavs-MacBook-Air.local>
2019-12-19 13:59:57 +0530

    arrays and 2d arrays

--- Lecture 5/spiralPrint.cpp
+++ Lecture 5/spiralPrint.cpp
@@ -11,5 +11,42 @@ int main(){
 					{25,26,27,28,29,30},
 					{31,32,33,34,35,36}};
 
+		int minRow = 0;
+		int minCol = 0;
+		int maxRow = 5;
+		int maxCol = 5;
+
+		int numOfElements = (6*6);
+
+		int count = 0;
+
+		while(count<numOfElements){
+
+			for(int i=minCol;i<=maxCol;i++){
+				cout<<arr[minRow][i]<<" ";
+				count++;
+			}
+			minRow++;
+
+			for(int i=minRow;i<=maxRow;i++){
+				cout<<arr[i][maxCol]<<" ";
+				count++;
+			}
+			maxCol--;
+
+			for(int i = maxCol;i>=minCol;i--){
+				cout<<arr[maxRow][i]<<" ";
+				count++;
+			}
+			maxRow--;
+
+			for(int i = maxRow;i>=minRow;i--){
+				cout<<arr[i][minCol]<<" ";
+				count++;
+			}
+			minCol++;
+		}
+		cout<<endl;
+
 	return 0;
 }
