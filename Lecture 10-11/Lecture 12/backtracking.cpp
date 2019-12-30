0fa36181516035a4edee7d7cff6e0a893bb8254e
Pranav Khandelwal <pranavkhandelwal@Pranavs-MacBook-Air.local>
2019-12-30 11:07:30 +0530

    backtracking

---
+++ Lecture 10-11/Lecture 12/backtracking.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+
+using namespace std;
+
+bool isSafe(int board[4][4],int row,int col,int n){
+
+	for(int i=0;i<row;i++){
+		if(board[i][col]){
+			return false;
+		}
+	}
+
+	int x = row;
+	int y = col;
+
+	while(x>=0 and y<n){
+		if(board[x][y]){
+			return false;
+		}
+		x--;
+		y++;
+	}
+
+	x = row;
+	y = col;
+
+	while(x>=0 and y>=0){
+		if(board[x][y]){
+			return false;
+		}
+		x--;
+		y--;
+	}
+
+	return true;
+}
+
+bool NQueens(int board[4][4],int row,int n){
+	if(row==n){
+
+		for(int i=0;i<n;i++){
+			for(int j=0;j<n;j++){
+				if(board[i][j]){
+					cout<<"Q ";
+				}else{
+					cout<<"_ ";
+				}
+			}
+			cout<<endl;
+		}
+
+		cout<<"*****************"<<endl;
+
+		return true;
+	}
+
+	for(int col=0;col<n;col++){
+
+		if(isSafe(board,row,col,n)){
+
+			board[row][col] = 1;
+
+			bool rest_of_the_queens = NQueens(board,row+1,n);
+
+			if(rest_of_the_queens){
+				return true;	
+			}
+
+			board[row][col] = 0;
+		}
+
+	}
+
+	return false;
+}
+
+int main(){
+
+
+	int board[4][4] = {0};
+	int n =4;
+
+	cout<<NQueens(board,0,n)<<endl;
+
+	return 0;
+}
