06ca6dc35cbc057b61daf6e0b3ff243461037c28
Pranav Khandelwal <pranavkhandelwal@Pranavs-MacBook-Air.local>
2020-01-02 11:18:48 +0530

    backtracking

---
+++ Lecture 13/divideAndConquer.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int binarySearch(int arr[],int n,int data){
+
+	int start = 0;
+	int end = n-1;
+
+	while(start<=end){
+
+		int mid = (start + end)/2;
+
+		if(data==arr[mid]){
+			return mid;
+		}else if(arr[mid] > data){
+			end = mid - 1;
+		}else{
+			start = mid + 1;
+		}
+	}
+
+	return -1;
+}
+
+int upperBound(int arr[],int n,int data){
+	
+}
+
+int main(){
+
+	int arr[] = {1,2,3,4,5,6,7,8,9};
+	int n = 9;
+	int data = 8;
+
+	cout<<binarySearch(arr,n,data)<<endl;
+
+	return 0;
+}
