1b186ff1e2410782b5950fe4ea499745417ea9d3
khandelwalpranav05 <khandelwalpranav1996@gmail.com>
2020-01-11 11:03:14 +0530

    stack

--- Lecture 17/questions.cpp
+++ Lecture 17/questions.cpp
@@ -27,11 +27,43 @@ bool balancedParanthesis(string str){
 	return s.empty();
 }
 
+bool isDuplicate(string str){
+
+	stack<char> s;
+
+	for(int i=0;i<str.length();i++){
+
+		char ch = str[i];
+
+		if(ch!=')'){
+			s.push(ch);
+		}else{
+
+			if(s.top()=='('){
+				return true;
+			}
+
+			while(s.top()!='('){
+				s.pop();
+			}
+			s.pop();
+
+		}
+	}
+
+	return false;
+
+}
+
 int main(){
 
-	cout<<balancedParanthesis("(())))((")<<endl;
-	cout<<balancedParanthesis("(())")<<endl;
-	cout<<balancedParanthesis("(())(")<<endl;
+	// cout<<balancedParanthesis("(())))((")<<endl;
+	// cout<<balancedParanthesis("(())")<<endl;
+	// cout<<balancedParanthesis("(())(")<<endl;
+
+	cout<<isDuplicate("(((a+(b))+c+d))")<<endl;
+	cout<<isDuplicate("((a+b)+(c+d))")<<endl;
+	cout<<isDuplicate("(((a+(b)))+(c+d))")<<endl;
 
 	return 0;
 }
