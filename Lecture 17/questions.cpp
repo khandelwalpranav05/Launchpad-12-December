861279ed52e7d08226a56392ae060ad578a077e5
khandelwalpranav05 <khandelwalpranav1996@gmail.com>
2020-01-17 12:49:08 +0530

    stack and queue

--- Lecture 17/questions.cpp
+++ Lecture 17/questions.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <stack>
+#include <queue>
+#include <deque>
 
 using namespace std;
 
@@ -173,6 +175,70 @@ int histogramArea(int arr[],int n){
 	return maxArea;
 }
 
+void firstNonRepeatingCharacter(){
+
+	queue<char> q;
+	int freq[256] = {0};
+
+	char ch;
+	cin>>ch;
+
+	while(ch!='!'){
+
+		q.push(ch);
+		freq[ch]++;
+
+		while(!q.empty()){
+			char check = q.front();
+
+			if(freq[check] > 1){
+				q.pop();
+			}else{
+				cout<<check<<" ";
+				break;
+			}
+		}
+
+		if(q.empty()){
+			cout<<(-1)<<" ";
+		}
+
+		cin>>ch;
+	}
+
+	cout<<endl;
+}
+
+void slidingWindow(int arr[],int n,int k){
+
+	deque<int> q;
+
+	int i = 0;
+
+	while(i<k){
+		while(!q.empty() and arr[q.back()] < arr[i]){
+			q.pop_back();
+		}
+		q.push_back(i);
+		i++;
+	}
+
+	for(;i<n;i++){
+		cout<<arr[q.front()]<<" ";
+
+		if(!q.empty() and q.front()<= i - k){
+			q.pop_front();
+		}
+
+		while(!q.empty() and arr[q.back()] < arr[i]){
+			q.pop_back();
+		}
+		q.push_back(i);
+	}
+
+	cout<<arr[q.front()]<<endl;
+}
+
 int main(){
 
 	// cout<<balancedParanthesis("(())))((")<<endl;
@@ -198,10 +264,18 @@ int main(){
 
 	// nextSmaller(arr,n);
 
-	int arr[] = {2,3,4,1,3};
-	int n = 5;
+	// int arr[] = {2,3,4,1,3};
+	// int n = 5;
 
-	cout<<histogramArea(arr,n)<<endl;
+	// cout<<histogramArea(arr,n)<<endl;
+
+	// firstNonRepeatingCharacter();
+
+	int arr[] = {2,3,1,4,6,5,3,0,1};
+	int n = 9;
+	int k = 3;
+
+	slidingWindow(arr,n,k);
 
 	return 0;
 }
